8e0dc7ad5d36d95665eff9c587f898ab7ea896f7
Chris Chua <chris@thousandeyes.com>
2013-10-02 15:36:48 -0700

    jQuery: Add support for circle shape

--- shapetext.js
+++ shapetext.js
@@ -11,8 +11,15 @@ $.fn.shapeText = function () {
     // Set up text field
     var d3text = setupTextField($element);
 
-    // Get position of element
-    var x = +$element.attr('x'), y = +$element.attr('y');
+    // Set up update functions depending on type of element
+    var updateSizeFn;
+    if ($element.is('rect')) {
+      updateSizeFn = rectUpdateSizeFn(d3element, d3text);
+    } else if ($element.is('circle')) {
+      updateSizeFn = circleUpdateSizeFn(d3element, d3text);
+    } else {
+      throw new Error('shapeText called on unsupported element');
+    }
 
     var changeFn = function () {
       // Update text
@@ -22,12 +29,7 @@ $.fn.shapeText = function () {
       var bbox = d3text.node().getBBox();
 
       // Update sizes
-      d3text
-        .attr('x', x)
-        .attr('y', y + bbox.height);
-      d3element
-        .attr('width', bbox.width)
-        .attr('height', bbox.height + 5);
+      updateSizeFn(bbox);
     };
 
     $model.on('input change', changeFn);
@@ -45,6 +47,39 @@ $.fn.shapeText = function () {
         .attr('y', bbox.y);
 
     return d3text;
+  }
+
+  function rectUpdateSizeFn(d3element, d3text) {
+
+    // Get position of element
+    var x = +d3element.attr('x'), y = +d3element.attr('y');
+
+    function updateSizeFn(bbox) {
+      d3text
+        .attr('x', x)
+        .attr('y', y + bbox.height);
+      d3element
+        .attr('width', bbox.width)
+        .attr('height', bbox.height + 5);
+    }
+
+    return updateSizeFn;
+  }
+
+  function circleUpdateSizeFn(d3element, d3text) {
+
+    // Get position of element
+    var x = +d3element.attr('cx'), y = +d3element.attr('cy');
+
+    function updateSizeFn(bbox) {
+      d3text
+        .attr('x', x - bbox.width / 2)
+        .attr('y', y + 5);
+      d3element
+        .attr('r', bbox.width / 2 + 5);
+    }
+
+    return updateSizeFn;
   }
 
 };
