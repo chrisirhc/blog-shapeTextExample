6a39de35aac9137a6e8d227df821ccd5572a0c4e
David Souther <davidsouther@gmail.com>
2013-10-19 11:17:45 -0400

    Removed explicit call to shapeText.

--- tests.js
+++ tests.js
@@ -1,5 +1,4 @@
 test("binding basics", function(){
-	$('.text-from-model').shapeText();
 	var input = $('#myText'),
 		rect = $('svg > rect'),
 		circle = $('svg > circle');
@@ -17,14 +16,4 @@ test("binding basics", function(){
 	equal(rect.attr('width'), 61);
 	equal(rect.attr('height'), 23);
 	equal(circle.attr('r'), 35.5);
-});
-
-test('exceptional svg', function(){
-	try {
-		$('<svg><ellipse /></svg>').find('ellipse').shapeText();
-		equal(false, true, 'Should have thrown exception');
-	}
-	catch (e) {
-		equal(e.message, 'shapeText called on unsupported element');
-	}
 });
