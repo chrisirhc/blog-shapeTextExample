4401fcd0b6d285ce34f5bc746b1b9bec7fe5cc9a
David Souther <davidsouther@gmail.com>
2013-10-19 11:03:08 -0400

    Test circle.

--- tests.js
+++ tests.js
@@ -1,12 +1,20 @@
 test("binding basics", function(){
 	$('.text-from-model').shapeText();
-	var input = $('#myText'), rect = $('svg > rect');
+	var input = $('#myText'),
+		rect = $('svg > rect'),
+		circle = $('svg > circle');
+
+	equal(circle.attr('r'), 0);
+
 	input.val('Foo').change();
-	equal($('svg > text').text(), 'Foo');
+	equal($('text:nth-of-type(1)').text(), 'Foo');
 	equal(rect.attr('width'), 28);
 	equal(rect.attr('height'), 23);
+	equal(circle.attr('r'), 19);
+
 	input.val('BizBang').change();
-	equal($('svg > text').text(), 'BizBang');
+	equal($('text:nth-of-type(2)').text(), 'BizBang');
 	equal(rect.attr('width'), 61);
 	equal(rect.attr('height'), 23);
+	equal(circle.attr('r'), 35.5);
 });
