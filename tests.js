46c319b26543de883ae5896ae23f8433a5c08f39
David Souther <davidsouther@gmail.com>
2013-10-19 21:01:48 -0400

    Test by forcing directives through a $compile/$digest cycle.

--- tests.js
+++ tests.js
@@ -1,19 +1,44 @@
-test("binding basics", function(){
-	var input = $('#myText'),
-		rect = $('svg > rect'),
-		circle = $('svg > circle');
+var fixture = $('#qunit-fixture'),
+	$injector = angular.injector(['ng', 'myMod']),
+	$compile = $injector.get('$compile'),
+	$rootScope = $injector.get('$rootScope');
 
-	equal(circle.attr('r'), 0);
+test("binding: Foo Rect", function(){
+	var scope = $rootScope.$new();
+	scope.text = "Foo";
+	var svg = $compile(
+		'<svg width="500" height="200">\
+			<rect shape-text="{{ text }}" x="0" y="20" fill="lightblue" />\
+		</svg>')(scope);
+	fixture.append(svg);
+	scope.$digest();
+	equal(svg.find('rect').attr('width'), 28);
+	equal(svg.find('rect').attr('height'), 23);
+});
 
-	input.val('Foo').change();
-	equal($('text:nth-of-type(1)').text(), 'Foo');
-	equal(rect.attr('width'), 28);
-	equal(rect.attr('height'), 23);
-	equal(circle.attr('r'), 19);
+test("binding: BizBang Circle", function(){
+	var scope = $rootScope.$new();
+	scope.text = "BizBang";
+	var svg = $compile(
+		'<svg width="500" height="200">\
+			<circle shape-text="{{ text }}" cx="50" cy="50" r="0" fill="lightgreen" />\
+		</svg>')(scope);
+	fixture.append(svg);
+	scope.$digest();
+	equal(svg.find('circle').attr('r'), 35.5);
+});
 
-	input.val('BizBang').change();
-	equal($('text:nth-of-type(2)').text(), 'BizBang');
-	equal(rect.attr('width'), 61);
-	equal(rect.attr('height'), 23);
-	equal(circle.attr('r'), 35.5);
+test('binding: BeebleBobble (both)', function(){
+	var scope = $rootScope.$new();
+	scope.text = "BeebleBobble";
+	var svg = $compile(
+		'<svg width="500" height="200">\
+			<rect shape-text="{{ text }}" x="0" y="20" fill="lightblue" />\
+			<circle shape-text="{{ text }}" cx="50" cy="50" r="0" fill="lightgreen" />\
+		</svg>')(scope);
+	fixture.append(svg);
+	scope.$digest();
+	equal(svg.find('rect').attr('width'), 102);
+	equal(svg.find('rect').attr('height'), 23);
+	equal(svg.find('circle').attr('r'), 56);
 });
